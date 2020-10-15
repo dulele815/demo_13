74d9a60dc6c716991c4b182a55c67d8be2b79a44
dulele815 <dulele815@163.com>
2020-10-15 23:46:29 +0800

    2020-10-15.dulele815

--- src/main/java/com/example/demo13/bean/Apple.java
+++ src/main/java/com/example/demo13/bean/Apple.java
@@ -1,8 +1,16 @@
 package com.example.demo13.bean;
 
-public class Apple {
+import java.io.Serializable;
 
+public class Apple implements Serializable {
+
+    private static final long serialVersionUID = 674971275430659319L;
     public String name;
     public String id;
     public String addaaa;
+
+    public void test() {
+
+    }
+
 }
